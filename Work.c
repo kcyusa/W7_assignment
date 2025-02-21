6a331cfe8ba65aebc51620674b683eb3459a1a63
cyusakeny <gasananike@gmail.com>
2025-02-21 18:40:29 +0200

    fix first program

--- Work.c
+++ Work.c
@@ -2,24 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define EMAIL_COUNT 10
-#define MAX_EMAIL_LENGTH 100 
+#define INITIAL_EMAIL_COUNT 10
+#define SHRUNK_EMAIL_COUNT 6
+#define MAX_EMAIL_LENGTH 100
 
 int main()
 {
     char **emails;
     int i;
 
-
-    emails = (char **)malloc(EMAIL_COUNT * sizeof(char *));
+    emails = (char **)malloc(INITIAL_EMAIL_COUNT * sizeof(char *));
     if (emails == NULL)
     {
         printf("Memory allocation failed\n");
         return 1;
     }
 
-
-    for (i = 0; i < EMAIL_COUNT; i++)
+    for (i = 0; i < INITIAL_EMAIL_COUNT; i++)
     {
         emails[i] = (char *)malloc(MAX_EMAIL_LENGTH * sizeof(char));
         if (emails[i] == NULL)
@@ -29,22 +28,37 @@ int main()
         }
     }
 
-   
     printf("Enter 10 email addresses:\n");
-    for (i = 0; i < EMAIL_COUNT; i++)
+    for (i = 0; i < INITIAL_EMAIL_COUNT; i++)
     {
         printf("Email %d: ", i + 1);
         scanf("%99s", emails[i]);
     }
 
-   
     printf("\nStored Email Addresses:\n");
-    for (i = 0; i < EMAIL_COUNT; i++)
+    for (i = 0; i < INITIAL_EMAIL_COUNT; i++) {
+        printf("%d: %s\n", i + 1, emails[i]);
+    }
+
+    for (i = SHRUNK_EMAIL_COUNT; i < INITIAL_EMAIL_COUNT; i++)
+    {
+        free(emails[i]);
+    }
+
+    emails = (char **)realloc(emails, SHRUNK_EMAIL_COUNT * sizeof(char *));
+    if (emails == NULL)
+    {
+        printf("Memory reallocation failed\n");
+        return 1;
+    }
+
+    printf("\nStored Email Addresses (After Shrinking to 6):\n");
+    for (i = 0; i < SHRUNK_EMAIL_COUNT; i++)
     {
         printf("%d: %s\n", i + 1, emails[i]);
     }
 
-    for (i = 0; i < EMAIL_COUNT; i++)
+    for (i = 0; i < SHRUNK_EMAIL_COUNT; i++)
     {
         free(emails[i]);
     }
@@ -52,4 +66,3 @@ int main()
 
     return 0;
 }
-
